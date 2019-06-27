713b118e19eb17e2444a1b5e6eef9e70b5ba4a67
whc <917763922@qq.com>
2019-06-27 16:57:01 +0800

    test1

--- src/HelloDemo.java
+++ src/HelloDemo.java
@@ -1,6 +1,8 @@
 public class HelloDemo {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("hello");
             System.out.println("hello");
+        }
     }
 }
