7002d540d8aed43af75809b9b2ce90c5ce0a9330
whc <917763922@qq.com>
2019-06-29 15:05:18 +0800

    合并测试22第一次提交 11.59

--- src/HelloDemo.java
+++ src/HelloDemo.java
@@ -1,6 +1,6 @@
 public class HelloDemo {
     public static void main(String[] args) {
-        for (int i = 0; i < 14; i++) {
+        for (int i = 0; i < 4; i++) {
             System.out.println("zuixinban");
         }
         System.out.println("合并测试22");
